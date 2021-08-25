4033e05c183e0f92836a73029c6bbd9cea8be272
Marco Steinhoff <steinhoff@locity.de>
2021-08-25 21:40:48 +0200

    basic project setup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   stories: [],
   addons: ['@storybook/addon-essentials'],
@@ -13,5 +15,35 @@ module.exports = {
         ],
       ],
     };
+  },
+  webpackFinal: async (config, { configType }) => {
+    // Make whatever fine-grained changes you need
+
+    // config.module.rules = config.module.rules.map((rule) => {
+    //   if ('.scss'.match(rule.test)) {
+    //     rule.use = rule.use.map((use) => {
+    //       if (use.loader.includes('sass-loader')) {
+    //         use.options = {
+    //           sassOptions: {
+    //             includePaths: ['libs/components/src/assets/generated'],
+    //           },
+    //         }
+    //       }
+    //       return use
+    //     })
+    //   }
+    //   return rule
+    // })
+
+    const tsPaths = new TsconfigPathsPlugin({
+      configFile: './tsconfig.base.json',
+    });
+
+    config.resolve.plugins
+      ? config.resolve.plugins.push(tsPaths)
+      : (config.resolve.plugins = [tsPaths]);
+
+    // Return the altered config
+    return config;
   },
 };
--- libs/frontend/.storybook/main.js
+++ libs/frontend/.storybook/main.js
@@ -1,9 +1,30 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const rootMain = require('../../../.storybook/main');
 
-// Use the following syntax to add addons!
-// rootMain.addons.push('');
-rootMain.stories.push(
-  ...['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)']
-);
+module.exports = {
+  ...rootMain,
 
-module.exports = rootMain;
+  core: { ...rootMain.core },
+
+  stories: [
+    ...rootMain.stories,
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [...rootMain.addons],
+  webpackFinal: async (config, { configType }) => {
+    // apply any global webpack configs that might have been specified in .storybook/main.js
+    if (rootMain.webpackFinal) {
+      config = await rootMain.webpackFinal(config, { configType });
+    }
+    const tsPaths = new TsconfigPathsPlugin({
+      configFile: './tsconfig.base.json',
+    });
+
+    config.resolve.plugins
+      ? config.resolve.plugins.push(tsPaths)
+      : (config.resolve.plugins = [tsPaths]);
+
+    return config;
+  },
+};
