5004a2da02f8bb2ace1a2c2b6e843f4cafab0cc7
Marco Steinhoff <steinhoff@locity.de>
2021-10-01 15:29:01 +0200

    add post css

--- libs/frontend/stencil.config.ts
+++ libs/frontend/stencil.config.ts
@@ -1,10 +1,27 @@
 import { Config } from '@stencil/core'
+import { postcss } from '@stencil/postcss'
 import { sass } from '@stencil/sass'
+import cssnano from 'cssnano'
 
 export const config: Config = {
   namespace: 'frontend',
   taskQueue: 'async',
-  plugins: [sass()],
+  plugins: [
+    sass(),
+    postcss({
+      plugins: [
+        cssnano({
+          preset: [
+            'default',
+            {
+              autoprefixer: { browsers: 'last 2 versions', add: true },
+              zindex: false,
+            },
+          ],
+        }),
+      ],
+    }),
+  ],
   globalStyle: `src/global.scss`,
   outputTargets: [
     {
