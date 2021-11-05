068deb43cd7222138290efa040d7b8ed8cc2ad90
Marco Steinhoff <steinhoff@locity.de>
2021-11-05 21:37:07 +0100

    ducktape frontend tests. #11

--- libs/frontend/src/components/toast/toast.component.spec.tsx
+++ libs/frontend/src/components/toast/toast.component.spec.tsx
@@ -1,32 +1,22 @@
 import { h } from '@stencil/core'
 import { newSpecPage } from '@stencil/core/testing'
-import { mapTo, timer } from 'rxjs'
+import { Toast } from '@typings'
+import { Subject } from 'rxjs'
 import { ToastComponent } from './toast.component'
 
 describe('ToastComponent', () => {
-  it('renders', async () => {
-    const page = await newSpecPage({
-      components: [ToastComponent],
-      autoApplyChanges: true,
-      template: () => <tf-toast />,
-    })
-
-    expect(page.root).toMatchSnapshot()
-  })
-
   it('renders with values', async () => {
-    const msg$ = timer(0, 5000).pipe(
-      mapTo({
-        text: 'Hello World',
-        status: 'success',
-      })
-    )
+    const subject = new Subject<Toast>()
     const page = await newSpecPage({
       components: [ToastComponent],
       autoApplyChanges: true,
-      template: () => <tf-toast msg$={msg$} />,
+      template: () => <tf-toast msg$={subject} />,
     })
-
+    subject.next({
+      text: 'Hello World',
+      status: 'success',
+    })
+    subject.complete()
     expect(page.root).toMatchSnapshot()
   })
 })
