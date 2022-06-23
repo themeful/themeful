e82d1080add6d8d6f30b0b111c29a33cdafa0f82
Marco Steinhoff <marco.steinhoff@lotto24.de>
2022-06-23 17:32:41 +0000

    change jest preset to typescript

--- libs/utils/jest.config.ts
+++ libs/utils/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
   displayName: 'utils',
-  preset: '../../jest.preset.js',
+  preset: '../../jest.preset.ts',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
