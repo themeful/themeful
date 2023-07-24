cbc2ac70cd059a65dde1c55e2870aa127854dd88
Marco Steinhoff <steinhoff@locity.de>
2023-07-24 03:05:47 +0000

    chore(deps): update nrwl

--- libs/ui-lib/.storybook/main.ts
+++ libs/ui-lib/.storybook/main.ts
@@ -1,16 +1,22 @@
 import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin'
 import { rootMain, StorybookOptions, WebPackFinalConfig } from '../../../.storybook/main'
-
 module.exports = {
   ...rootMain,
-
-  core: { ...rootMain.core },
-
+  core: {
+    ...rootMain.core,
+  },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-essentials', ...(rootMain.addons ?? [])],
   babel: async (options: StorybookOptions) => ({
     ...options,
-    presets: [['@babel/typescript', { jsxPragma: 'h' }]],
+    presets: [
+      [
+        '@babel/typescript',
+        {
+          jsxPragma: 'h',
+        },
+      ],
+    ],
   }),
   webpackFinal: async (config: WebPackFinalConfig, options: StorybookOptions) => {
     if (rootMain.webpackFinal) {
@@ -19,11 +25,12 @@ module.exports = {
     const tsPaths = new TsconfigPathsPlugin({
       configFile: './tsconfig.base.json',
     })
-
     config.resolve.plugins
       ? config.resolve.plugins.push(tsPaths)
       : (config.resolve.plugins = [tsPaths])
-
     return config
+  },
+  docs: {
+    autodocs: true,
   },
 }
